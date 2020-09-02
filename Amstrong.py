87d952fe3b1cd2a968210531ede45a4cf582dfc8
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-09-02 16:26:22 +0530

    Update Amstrong.py

--- Amstrong.py
+++ Amstrong.py
@@ -1,8 +1,8 @@
-#Checking whether amstrong number or not
+#Checking whether Amstrong number or not
 print("121910313006","Kadiyala Rohit Bharadwaj")
 n=int(input("Enter number of digits:"))
 x=int(input("enter a number:"))
-temp=x
+temp=x #Assigning x value to a temporary variable temp
 sum=0
 sum1=0
 while(temp!=0):
@@ -11,8 +11,8 @@ while(temp!=0):
     sum1=sum1+rem
     temp=temp//10
 print("sum of cubes of the digits in the number:",sum)
-if(x==sum):#Checking whether sum and n are equal or not
-    print(x," is amstrong number")
+if(x==sum):#Checking whether sum and x are equal or not
+    print(x," is an amstrong number")
 else:
-    print(x," is not a amstrong number")
+    print(x," is not an amstrong number")
 print("Sum of digits in the number is:",sum1)
