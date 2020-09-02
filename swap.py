7c4c679bbc53865d56aec78b0c8344a332969524
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-09-02 12:26:48 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,7 +1,7 @@
 print("121910313006","Kadiyala Rohit Bharadwaj")
 #Swapping two numbers
-a=int(input("Enter a number:"))
-b=int(input("Enter a number:"))
+a=int(input("Enter a number:"))#Var1
+b=int(input("Enter a number:"))#Var2
 print("Before swapping:",a,b)
 temp=a
 a=b
