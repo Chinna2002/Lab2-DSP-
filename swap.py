d80aeb020bca5509d2641c06cf1f383fa4474ef2
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-09-15 12:16:50 +0530

    Delete swap.py

--- swap.py
+++
@@ -1,9 +0,0 @@
-print("121910313006","Kadiyala Rohit Bharadwaj")
-#Swapping two numbers
-a=int(input("Enter a number:"))#Var1
-b=int(input("Enter a number:"))#Var2
-print("Before swapping:",a,b)
-temp=a
-a=b
-b=temp
-print("After swapping:",a,b)
