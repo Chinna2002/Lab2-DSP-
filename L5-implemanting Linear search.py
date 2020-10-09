f72bb412bcd26d6a2540455c8d8c6d022daa0d4c
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-10-09 11:18:17 +0530

    Update L5-implemanting Linear search.py

--- L5-implemanting Linear search.py
+++ L5-implemanting Linear search.py
@@ -1,6 +1,7 @@
 #Linear Search
 print("121910313006","Kadiyala Rohit Bharadwaj")
 l=[1,2,21,8,9,77,50]#array
+flag=0#initialising to zer0
 search=int(input("enter an element to be searched:"))
 for i in range(len(l)):
     if(search==l[i]):
