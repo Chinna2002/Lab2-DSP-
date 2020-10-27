5333d6232434b982dea191c017912d7cea06fa23
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-10-27 15:21:11 +0530

    Update L9-Operations on stack with user choice.py

--- L9-Operations on stack with user choice.py
+++ L9-Operations on stack with user choice.py
@@ -3,42 +3,29 @@ class Stack:
     def __init__(self):
         self.items = []
 
-    def push(self, data,l):
-        if data not in self.items and len(self.items) <= l:
-            self.items.append(data)
-            return
-        print("Overflow")
-        return
+    def push(self, data):
+        self.items.append(data)
 
     def pop(self):
-        if len(self.items)>0:
-            x=self.items[-1]
-            self.items.pop()
-            return x
-        print("Underflow")
-        return
-    def length(self):
-        return len(self.items)
+        if s.items!=[]: return self.items.pop()
 
     def printstack(self):
-        if(len(self.items) == 0):
-            print("UnderFlow Condition")
-        else:
-            print(self.items)
+        print(self.items)
 s=Stack()
-n = int(input("Enter size of the stack:"))
 i=True
 while(i):
     print("1.Push","2.Pop","3.Display","4.Exit")
     choice = int(input("Select an Option:"))
     if(choice==1):
-        s.push(int(input("Enter Element:")),n)
-    elif choice == 2:
-        print(s.pop())
-    elif choice == 3:
+        s.push(int(input("Enter Element:")))
+    elif (choice == 2):
+        print("The Stack Element popped is:",s.pop())
+    elif (choice == 3):
+        print("The Stack is:")
         s.printstack()
-    elif choice== 4:
+    elif (choice== 4):
         i = False
+        print("Task Terminated")
     else:
         print("Enter a valid input ")
 
