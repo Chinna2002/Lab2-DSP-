9eb731ef55aa4e2e5a8381418fc1ebb31f1f9706
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-11-03 10:40:09 +0530

    Update L10-Postfix evaluation by stack.py

--- L10-Postfix evaluation by stack.py
+++ L10-Postfix evaluation by stack.py
@@ -1,38 +1,32 @@
-print("121910313006","Kadiyala Rohit Bhardawaj")
-#Evaluating Postfix Expression
-class Evaluate:
-    def __init__(self):
-        self.top = -1
-        self.array = []
-    def isEmpty(self):
-      if self.top == -1:
-          return True
-      else:
-          return False
-    def peek(self):
-        return self.array[-1]
-    def pop(self):
-        if not self.isEmpty():
-            self.top -= 1
-            return self.array.pop()
-        else:
-            print("Empty")
-    def push(self, op):
-        self.top += 1
-        self.array.append(op)
-    def evaluatePostfix(self, exp):#Function for postfix evaluation
-        for i in exp:
-            if i.isdigit():
-                self.push(i)
-            else:
-                val1 = self.pop()
-                val2 = self.pop()
-                self.push(str(eval(val2 + i + val1)))
-        return int(self.pop())
+print("121910313006","Kadiyala Rohit Bharadwaj")
+def postfix(exp):
+    exp = exp.split()
+    s=[]
+    for i in exp:
+        if i.isdigit():
+            s.append(int(i))
+        elif i == "+":
+            a = s.pop()
+            b = s.pop()
+            s.append(int(a)+int(b))
+        elif i == "-":
+            a = s.pop()
+            b = s.pop()
+            s.append(int(b)-int(a))
+        elif i == "*":
+            a = s.pop()
+            b = s.pop()
+            s.append(int(a)*int(b))
+        elif i == "/":
+            a = s.pop()
+            b = s.pop()
+            s.append(int(b)/int(a))
+    return s.pop()
+exp = input("Enter an expression:")
+val = postfix(exp)
+print("Required value is : ")
+print(val)
 
-x=input("Enter an expression:")
-s=Evaluate()
-print(s.evaluatePostfix(x))
 
 
 
