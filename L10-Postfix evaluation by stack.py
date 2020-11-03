4ac210705b4f176facc253baa9e828c83474bba9
Chinna2002 <70188930+Chinna2002@users.noreply.github.com>
2020-11-03 10:55:10 +0530

    Update L10-Postfix evaluation by stack.py

--- L10-Postfix evaluation by stack.py
+++ L10-Postfix evaluation by stack.py
@@ -3,29 +3,16 @@ def postfix(exp):
     s=[]
     for i in exp:
         if i.isdigit():
-            s.append(int(i))
-        elif i == "+":
+            s.append(i)
+        else:
             a = s.pop()
             b = s.pop()
-            s.append(int(a)+int(b))
-        elif i == "-":
-            a = s.pop()
-            b = s.pop()
-            s.append(int(b)-int(a))
-        elif i == "*":
-            a = s.pop()
-            b = s.pop()
-            s.append(int(a)*int(b))
-        elif i == "/":
-            a = s.pop()
-            b = s.pop()
-            s.append(int(b)/int(a))
+            s.append(str(eval(a+i+b)))
     return s.pop()
-exp = input("Enter an expression:")
+exp = str(input("Enter an expression:"))
 val = postfix(exp)
 print("Required value is : ")
 print(val)
-
 
 
 
